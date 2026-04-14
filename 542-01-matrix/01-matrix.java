85a772d1891eea2e3f331947b37e5aa1ae3a43ed
Anil kumar <148522556+Anilllllllll@users.noreply.github.com>
2026-04-14 17:45:54 +0530

    Time: 13 ms (92.95%) | Memory: 48.9 MB (74.75%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -7,36 +7,30 @@ class Solution {
         int m = mat[0].length;
 
         Queue<int[]> q = new LinkedList<>();
-
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-
                 if(mat[i][j] == 0){
                     q.add(new int[]{i, j});
-                }
-                else{
+                } else {
                     mat[i][j] = -1;
                 }
             }
         }
+        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
 
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-
+       
         while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
 
-            int[] node = q.poll();
-            int a = node[0];
-            int b = node[1];
+            for(int d = 0; d < 4; d++){
+                int nx = x + dir[d][0];
+                int ny = y + dir[d][1];
 
-            for(int[] d : dir){
-
-                int nr = a + d[0];
-                int nc = b + d[1];
-
-                if(nr >= 0 && nc >= 0 && nr < n && nc < m && mat[nr][nc] == -1){
-
-                    mat[nr][nc] = mat[a][b] + 1;
-                    q.add(new int[]{nr, nc});
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m && mat[nx][ny] == -1){
+                    mat[nx][ny] = mat[x][y] + 1;
+                    q.add(new int[]{nx, ny});
                 }
             }
         }

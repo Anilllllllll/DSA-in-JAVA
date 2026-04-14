69cb9cb5dad2b4e5627aa88d82572dd0612fe510
Anil kumar <148522556+Anilllllllll@users.noreply.github.com>
2026-04-14 16:49:24 +0530

    Time: 2 ms (90.26%) | Memory: 44.2 MB (44.59%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,43 +1,51 @@
+import java.util.*;
+
 class Solution {
     public int orangesRotting(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int fresh = 0;
         Queue<int[]> q = new LinkedList<>();
-        int fresh =0;
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j]==2){
-                   q.add(new int[]{i,j});
-                }
-                if(grid[i][j]==1){
-                    fresh++;
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) fresh++;
+                else if (grid[i][j] == 2) q.add(new int[]{i, j});
             }
         }
-        if(fresh==0){
-            return 0;
-        }
-        int min=0;
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-            int n = q.size();
-            while(n-->0){
-                int[] p = q.poll();
-                int row = p[0];
-                int col = p[1];
-                for(int[] d: dir){
-                    int nr = row+d[0];
-                    int nc = col+d[1];
 
-                    if(nr>=0 && nc>=0 && nr<grid.length && nc<grid[0].length && grid[nr][nc]==1){
+        if (fresh == 0) return 0;
+
+        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+        int time = 0;
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+            boolean check = false;
+
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                int x = curr[0];
+                int y = curr[1];
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = x + dir[d][0];
+                    int ny = y + dir[d][1];
+
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        q.add(new int[]{nx, ny});
                         fresh--;
-                        q.add(new int[]{nr,nc});
-                        grid[nr][nc]=2;
+                        check = true;
                     }
                 }
-
             }
-            min++;
+
+            if (check) time++;
         }
-        return (fresh==0)? min-1:-1;
+
+        return (fresh == 0) ? time : -1;
     }
-   
 }

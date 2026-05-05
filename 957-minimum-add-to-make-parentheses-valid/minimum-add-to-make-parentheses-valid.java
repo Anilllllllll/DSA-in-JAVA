038a35065918bb3a16f88491a8d4549b6f3bd4cb
Anil kumar <148522556+Anilllllllll@users.noreply.github.com>
2026-05-05 13:01:35 +0530

    Time: 2 ms (41.51%) | Memory: 42.7 MB (86.60%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        
+        int count=0;
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(st.isEmpty()){
+                st.push(ch);
+            }
+            else{
+                if(ch==')' && st.peek()=='('){
+                    st.pop();
+                }
+                else{
+                    st.push(ch);
+                }
+            }
+        }
+        return st.size();
+    }
+}

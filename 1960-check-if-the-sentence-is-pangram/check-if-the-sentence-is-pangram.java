a4f80887e03f4da0a3b106aecafee03bcba237e1
Anil kumar <148522556+Anilllllllll@users.noreply.github.com>
2026-05-07 22:53:38 +0530

    Time: 5 ms (10.22%) | Memory: 43.2 MB (19.22%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean checkIfPangram(String s) {
-        
 
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            map.put(ch,1);
+            map.put(ch, 1);
         }
-        char ch ='a';
-        while(ch<='z'){
-            if(!map.containsKey(ch)){
+
+        char ch = 'a';
+
+        while (ch <= 'z') {
+            if (!map.containsKey(ch)) {
                 return false;
             }
             ch++;
         }
+
         return true;
-
-
-
     }
-
 }

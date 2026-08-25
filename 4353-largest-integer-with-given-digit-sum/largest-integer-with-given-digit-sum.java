488f7e36e737ee8af6461fbf67429c205c1b772c
Anil kumar <148522556+Anilllllllll@users.noreply.github.com>
2026-08-25 16:34:04 +0530

    Time: 170 ms (5.64%) | Memory: 42.4 MB (71.47%) - LeetSync

--- 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.java
+++ 4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.java
@@ -1,34 +1,72 @@
 class Solution {
     public int largestInteger(int n, int s) {
 
-        if (s == 0) {
+        if(n==0 || s==0){
             return 0;
         }
-
+     
         int ans = -1;
-
-        int start = 1;
-        for (int i = 1; i < n; i++) {
-            start *= 10;
-        }
-
-        int end = start * 10 - 1;
-
-        for (int i = start; i <= end; i++) {
-
-            int sum = 0;
-            int m = i;
-
-            while (m != 0) {
-                sum += m % 10;
-                m /= 10;
-            }
-
-            if (sum == s) {
-                ans = i;
+        if(n==1){
+            for(int i=1; i<=9; i++){
+                
+                if(i==s){
+                    return i;
+                }
             }
         }
-
+        else if(n==2){
+            for(int i=10; i<100; i++){
+                int count = 0;
+                int m = i;
+                while(m!=0){
+                    count+=m%10;
+                    m=m/10;
+                }
+                if(count==s){
+                        ans =i;
+                    }
+            }
+        }
+        else if(n==3){
+            for(int i=100; i<=999; i++){
+                long count = 0;
+                int m = i;
+                while(m!=0){
+                    count+=m%10;
+                    m=m/10;
+                }
+                if(count==s){
+                        ans =i;
+                    }
+            }
+        }
+        else if(n==4){
+            for(int i=1000; i<=9999; i++){
+                long count = 0;
+                int m = i; 
+                while(m!=0){
+                    count+=m%10;
+                    m=m/10;
+                }
+                if(count==s){
+                        ans =i;
+                    }
+            }
+        }
+        else if(n==5){
+            for(int i=10000; i<=99999; i++){
+                long count = 0;
+                int m = i;
+                while(m!=0){
+                    count+=m%10;
+                    m=m/10;
+                }
+                if(count==s){
+                        ans =i;
+                    }
+            }
+            
+        }
         return ans;
     }
 }
